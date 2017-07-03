e256feb3cbd84c3d0edbe250ad664f2704f9486f
Brendan Davis <BrendanMD@gmail.com>
2017-07-02 19:50:55 -0700

    Can now sort images based on final ratio

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var totalViews = 0;
 var newRay = [1, 2, 3]; //placeholder numbers
 var oldRay = [3, 2, 1]; //placeholder numbers
 var img3 = document.getElementById('img3');
-var voteRat = 0
 var ids = [] //Need to populate. (Can do it)
 
 // if totalClicks >= 25 {}; Remove images (removeIm), add message & button to reveal chart (closingTime).
@@ -16,7 +15,7 @@ function Pic(name, path) {
   this.path = path;
   this.clickTally = 0;
   this.shownTally = 0;
-  this.voteRat = this.clickTally/this.shownTally
+  this.final = 0
   images.push(this)
 }
 
@@ -41,6 +40,29 @@ new Pic('usb', 'assets/usb.gif');
 new Pic('water-can.jpg', 'assets/water-can.jpg');
 new Pic('wine-glass.jpg', 'assets/wine-glass.jpg');
 
+function compare(a, b) {
+  if (a.final < b.final) {
+    return -1
+  } else if (a.final > b.final) {
+    return 1
+  } else {
+    return 0
+  }
+  }
+
+function calcRat() {
+  for (var c = 0; c <images.length; c++) {
+    if (images[c].shownTally === 0) {
+      images[c].final = 0
+    } else {
+      images[c].final = images[c].clickTally / images[c].shownTally
+    }
+    console.log(images[c].final)
+  }
+  images.sort(compare)
+}
+
+
 function handleClick(event) {
   console.log('Click click!')
   for(var i = 0; i < images.length; i++) {
@@ -53,9 +75,10 @@ function handleClick(event) {
       renderImage();
     }
   }
-  if (totalClicks === 25) {
+  if (totalClicks === 5) {
     img3.removeEventListener('click', handleClick);
     clearImages();
+    console.log(images);
   }
 }
 
@@ -71,7 +94,8 @@ function generateRay() { //Generates a new array & sets old array
     var thisPic = randomImg();
     console.log(thisPic);
     if (oldRay.indexOf(thisPic) === -1 && newRay.indexOf(thisPic) === -1) {
-      newRay.push(thisPic)
+      newRay.push(thisPic);
+      thisPic.shownTally++;
     }
     continue;
   }
@@ -108,7 +132,7 @@ function pullThing() {
   }
 }
 
-if (localStorage) {
+if (localStorage === true) {
   pullThing();
 }
 
